20b7f6971eb0349f9ff90f8be8f290f615f098a8
Yudhishthirsh <130043970+Yudhishthirsh@users.noreply.github.com>
2023-05-22 12:54:00 +0530

    commited

--- Website_visitors/getIPAddress.py
+++ Website_visitors/getIPAddress.py
@@ -1,5 +1,6 @@
 import csv
 from flask import Flask, request, render_template
+import parameter
 
 app = Flask(__name__)
 ip_addresses = []
@@ -8,10 +9,13 @@ ip_addresses = []
 def index():
     ip_address = request.headers.get('X-Forwarded-For', request.remote_addr)
     ip_addresses.append(ip_address)
-    with open('ip_addresses.csv', 'a', newline='') as file:
+    column_name = 'IPAddress'
+    with open(parameter.fileName, 'a', newline='') as file:
+        if file.tell() == 0:
+            file.write(column_name + '\n')
         writer = csv.writer(file)
         writer.writerow([ip_address])
-        return render_template('index9.html', ip_address=ip_address)
+        return render_template('index.html', ip_address=ip_address)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)
---
+++ Website_visitors/parameter.py
@@ -0,0 +1,6 @@
+import time
+# Define the Random file name
+# Get the current timestamp
+timestamp = time.strftime("%Y%m%d%H%M%S")
+fileName = timestamp + 'visitorsIP' + '.csv' #this the generated filename
+print("Your output file is: ",fileName )
